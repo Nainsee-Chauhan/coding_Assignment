49c59b7b4a9535ddcce8edea811982fd6a23e35c
Nainsee Chauhan <nainsee02@gmail.com>
2024-05-29 00:39:27 +0530

    update commit

--- client/src/components/CalcTime.js
+++ client/src/components/CalcTime.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const BASE_URL = env.process.BASE_URL
+const BASE_URL = process.env.BASE_URL
 
 const CalculateTime = () => {
   const [distance, setDistance] = useState("");
--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/calculate_distance', (req, res) => {
   });  
 });
 
-const PORT  = process.env.PORT || 8000;
+const PORT  = process.env.PORT || 8000;  
 
 app.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`);
